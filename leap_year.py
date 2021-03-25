cb3302458aa391c46df53aed06c4bc8d3673b872
Bino Relawan <relawanbino@gmail.com>
2021-03-25 21:39:00 +0700

    days to list

--- leap_year.py
+++ leap_year.py
@@ -15,21 +15,23 @@ def days_in_month(year, month):
 
     days = 0
     if is_year_leap(year) == True:
-        if month == 2:
-            days = 29
-        elif month == 4 or month == 6 or month == 9 or month == 11:
-            days = 30
-        else:
-            days = 31
-        print(days)
+        days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        # if month == 2:
+        #     days = 29
+        # elif month == 4 or month == 6 or month == 9 or month == 11:
+        #     days = 30
+        # else:
+        #     days = 31
+        print(days[month])
     else:
-        if month == 2:
-            days = 28
-        elif month == 4 or month == 6 or month == 9 or month == 11:
-            days = 30
-        else:
-            days = 31
-        print(days)
+        days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        # if month == 2:
+        #     days = 28
+        # elif month == 4 or month == 6 or month == 9 or month == 11:
+        #     days = 30
+        # else:
+        #     days = 31
+        print(days[month])
             
 if __name__ == "__main__":
-    days_in_month(1900, 2)
+    days_in_month(2000, 2)
